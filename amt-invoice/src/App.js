be49bc05b68e5c85e29c2d00c9f77acb26aa183e
benmarcjay <benmarcjayagulto@gmail.com>
2023-01-30 14:16:54 -0700

    removed ClientProfile import from App.js

--- amt-invoice/src/App.js
+++ amt-invoice/src/App.js
@@ -4,23 +4,22 @@ import Layout from "./components/Navigation/Layout";
 
 import Login from "./components/Login/Login";
 import HomePage from "./components/Home/Home";
-import ClientProfile from "./components/ClientProfile/ClientProfile";
 
 function App() {
   return (
-    // <Switch>
-    //   <Route path="/" exact>
-    //     <Login />
-    //   </Route>
+    <Switch>
+      <Route path="/" exact>
+        <Login />
+      </Route>
 
-    //   <Layout>
-    //     <Route path="/home" exact>
-    //       <HomePage />
-    //     </Route>
-    //   </Layout>
-    // </Switch>
+      <Layout>
+        <Route path="/home" exact>
+          <HomePage />
+        </Route>
+      </Layout>
+    </Switch>
     
-    <ClientProfile></ClientProfile>
+    
   );
 }
 
