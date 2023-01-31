8b559c40434251d49d0500499e188b5e7df92f34
Henry Pham <phamhuyhoang52888@gmail.com>
2023-01-31 13:08:39 -0700

    Retrieve data and form an object

--- amt-invoice/src/components/CreateProject/CreateProject.js
+++ amt-invoice/src/components/CreateProject/CreateProject.js
@@ -6,15 +6,82 @@ import CreateProjectAddOns from "./CreateProjectAddOns";
 import Button from "../UI/Button";
 
 import styles from "./CreateProject.module.css";
+import { useRef, useState } from "react";
 
 const CreateProject = () => {
+  const [cabinet, setCabinet] = useState("");
+  const [tile, setTile] = useState("");
+  const [counter, setCounter] = useState("");
+
+  function onCabinetChange(event) {
+    if (event.target.value === "true") {
+      setCabinet("true");
+    } else {
+      setCabinet("false");
+    }
+  }
+  function onCounterChange(event) {
+    if (event.target.value === "true") {
+      setCounter("true");
+    } else {
+      setCounter("false");
+    }
+  }
+  function onTileChange(event) {
+    if (event.target.value === "true") {
+      setTile("true");
+    } else {
+      setTile("false");
+    }
+  }
+
+  const projectNameRef = useRef();
+  const siteSuperPhoneRef = useRef();
+  const siteSuperNameRef = useRef();
+  const postalCodeRef = useRef();
+  const cityRef = useRef();
+  const addressRef = useRef();
+  const suiteRef = useRef();
+  const provinceRef = useRef();
+
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const projectName = projectNameRef.current.value;
+    const siteSuperPhone = siteSuperPhoneRef.current.value;
+    const siteSuperName = siteSuperPhoneRef.current.value;
+    const postalCode = postalCodeRef.current.value;
+    const city = cityRef.current.value;
+    const address = addressRef.current.value;
+    const suite = suiteRef.current.value;
+    const province = provinceRef.current.value;
+
+    const obj = {
+      projectName, siteSuperPhone, siteSuperName, postalCode, city, address, suite, province, cabinet, tile, counter
+    }
+    console.log(obj);
+  }
+
   return (
     <React.Fragment>
       <MainNavigation />
       <h1 className={styles.h1}>Create Project</h1>
-      <CreateProjectDetails />
-      <CreateProjectAddOns />
-      <Button>Create Project</Button>
+      <form onSubmit={submitHandler}>
+        <CreateProjectDetails 
+        projectNameRef={projectNameRef}
+        siteSuperNameRef={siteSuperNameRef}
+        siteSuperPhoneRef={siteSuperPhoneRef}
+        postalCodeRef={postalCodeRef}
+        cityRef={cityRef}
+        addressRef={addressRef}
+        suiteRef={suiteRef}
+        provinceRef={provinceRef}/>
+        <CreateProjectAddOns 
+        onCabinetChange={onCabinetChange}
+        onTileChange={onTileChange}
+        onCounterChange={onCounterChange}/>
+        <Button type="submit">Create Project</Button>
+      </form>
     </React.Fragment>
   );
 };
--- amt-invoice/src/components/CreateProject/CreateProjectDetails.js
+++ amt-invoice/src/components/CreateProject/CreateProjectDetails.js
@@ -3,14 +3,15 @@ import React from "react";
 import CardWithRadius from "../UI/CardWithRadius";
 import styles from "./CreateProjectDetails.module.css";
 
-const CreateProjectDetails = () => {
+
+const CreateProjectDetails = (props) => {
   return (
     <React.Fragment>
       <div className={styles.parent}>
         <div className={styles.left}>
           <CardWithRadius>
             <label>Project Name:</label>
-            <input type="text" className="project-name" />
+            <input type="text" className="project-name" ref={props.projectNameRef}/>
           </CardWithRadius>
           <CardWithRadius>
             <label>Site Super Phone:</label>
@@ -19,33 +20,34 @@ const CreateProjectDetails = () => {
               className="site-super-phone"
               placeholder="123-456-7890"
               pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              ref={props.siteSuperPhoneRef}
             />
           </CardWithRadius>
           <CardWithRadius>
             <label>Postal Code:</label>
-            <input type="text" className="postal-code" />
+            <input type="text" className="postal-code" ref={props.postalCodeRef}/>
           </CardWithRadius>
           <CardWithRadius>
             <label>City:</label>
-            <input type="text" className="city" />
+            <input type="text" className="city" ref={props.cityRef}/>
           </CardWithRadius>
         </div>
         <div className={styles.right}>
           <CardWithRadius>
             <label>Site Super Name:</label>
-            <input type="text" className="site-super-name" />
+            <input type="text" className="site-super-name" ref={props.siteSuperNameRef}/>
           </CardWithRadius>
           <CardWithRadius>
             <label>Address:</label>
-            <input type="text" className="address" />
+            <input type="text" className="address" ref={props.addressRef}/>
           </CardWithRadius>
           <CardWithRadius>
             <label>Suite/Building No.:</label>
-            <input type="number" className="building-no" min="0" />
+            <input type="number" className="building-no" min="0" ref={props.suiteRef}/>
           </CardWithRadius>
           <CardWithRadius>
             <label>Province:</label>
-            <input type="text" className="province" />
+            <input type="text" className="province" ref={props.provinceRef}/>
           </CardWithRadius>
         </div>
       </div>
