bdacf9d7833554f2bd5ec4a8912616f761f7444d
EvenSten <42704997+EvenSten@users.noreply.github.com>
2023-01-31 20:00:47 -0700

    Changing the index.css file to make the login page and nav bar look less like shit

--- amt-invoice/src/components/Home/Home.js
+++ amt-invoice/src/components/Home/Home.js
@@ -4,9 +4,7 @@ function HomePage(props) {
     return (
       <main>
         <h1>Hello There</h1>
-        <div>
-        <button onClick={props.handleLogout}>logout</button>
-      </div>
+        
       </main>
     );
   }
