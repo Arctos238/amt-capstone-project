23e4bf357609afb025adcd034fd93dd8b97e87c6
EvenSten <42704997+EvenSten@users.noreply.github.com>
2023-02-10 21:24:55 -0700

    Adding updated login page

--- amt-invoice/src/components/Login/LoginForm.js
+++ amt-invoice/src/components/Login/LoginForm.js
@@ -2,12 +2,48 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { GetEmployeeByUsername } from "../../services/EmployeeServices";
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Card from "../UI/Card";
 import classes from "../UI/Card.module.css";
 import styles from "./LoginForm.module.css";
 
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import InputLabel from '@mui/material/InputLabel';
+import Input from '@mui/material/Input';
+import IconButton from '@mui/material/IconButton';
+
+import InputAdornment from '@mui/material/InputAdornment';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FFFFFF',
+      
+    },
+    background: {
+      main: '#fabd44',
+      
+    },
+
+    text:{
+      main: '#FFFFFF'
+    }
+  },
+});
+
 const LoginForm = (props) => {
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
@@ -43,31 +79,58 @@ const LoginForm = (props) => {
 
   return (
     <div className={styles.formLogin}>
-      <label className={styles.label}>Username:</label>
+      <ThemeProvider theme={theme}>
       <Card className={styles.inputContainer}>
-        <input
-          className={styles.input}
-          type="text"
-          name="formUsername"
-          onChange={inputHandler}
-          value={username}
-        ></input>
+        
+        <TextField
+        id="input-with-icon-textfield"
+        type="text"
+        name="formUsername"
+        onChange={inputHandler}
+        value={username}
+        label="Username"
+        
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }}
+        variant="standard"
+      />
       </Card>
 
-      <label className={styles.label}>Password:</label>
+      
       <Card className={styles.inputContainer}>
-        <input
-          className={styles.password}
-          type="password"
-          name="formPassword"
-          onChange={inputHandler}
-          value={password}
-        ></input>
+        
+        <FormControl sx={{ m: 1, width: '23ch' }} variant="standard">
+          <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+          <Input
+            
+            name="formPassword"
+            onChange={inputHandler}
+            value={password}
+            id="standard-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="start">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
       </Card>
 
-      <button className={styles.submitButton} onClick={handleSubmit}>
-        Submit
-      </button>
+      
+      <Button variant="contained"  sx={{ bgcolor: 'background.main', color: 'text.main'}}className={styles.submitButton}onClick={handleSubmit}>Login</Button>
+      </ThemeProvider>
     </div>
   );
 };
