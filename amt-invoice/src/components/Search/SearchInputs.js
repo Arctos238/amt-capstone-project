af23325377a1cb46d599fc7e9e431d41868399ec
benmarcjay <benmarcjayagulto@gmail.com>
2023-02-23 16:59:41 -0700

    Styling on Invoice Finder

--- amt-invoice/src/components/Search/SearchInputs.js
+++ amt-invoice/src/components/Search/SearchInputs.js
@@ -1,19 +1,29 @@
 import React, { useEffect, useRef, useState } from "react";
 import CardWithRadius from "../UI/CardWithRadius";
 import Button from "../UI/Button";
+import buttonClass from "../UI/Button.module.css";
 import SearchResult from "./SearchResult";
 import { GetClientByFirstName } from "../../services/ClientServices";
 import classes from "../UI/CardWithRadius.module.css";
 import styles from "./SearchInputs.module.css";
 import Alert from "@mui/material/Alert";
 import Stack from "@mui/material/Stack";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import Card from "../UI/Card";
+import cardClass from "../UI/Card.module.css";
+import SearchIcon from "@mui/icons-material/Search";
+import Grid from "@mui/material/Grid";
 
 const SearchInputs = (props) => {
-  const [isValidSearch,setIsValidSearch] = useState(true);
+  const [isValidSearch, setIsValidSearch] = useState(true);
   const [showRadioButtons, setShowRadioButtons] = useState(false);
   const [selectedOption, setSelectedOption] = useState("invoiceId");
   const [searchInput, setSearchInput] = useState("");
   const [results, setResults] = useState([]);
+  const [noResult, setNoResult] = useState(false);
 
   const handleOptionChange = (e) => {
     setIsValidSearch(true);
@@ -21,101 +31,152 @@ const SearchInputs = (props) => {
   };
 
   const handleSearchInput = (e) => {
+    setNoResult(false);
+    setIsValidSearch(true);
     setSearchInput(e.target.value);
+    setResults([]);
   };
 
   const fetchData = async () => {
-    const data = await GetClientByFirstName(searchInput);
-    setResults(data);
+    try {
+      const data = await GetClientByFirstName(searchInput);
+      setResults(data);
+    } catch (error) {
+      console.log(error);
+      setNoResult(true);
+    }
   };
 
   const findHandler = () => {
-    if(searchInput === ""){
+    if (searchInput === "") {
       setIsValidSearch(false);
     } else {
-      fetchData();
+      try {
+        fetchData();
+      } catch (error) {
+        console.log(error);
+      }
     }
+    console.log(results.length);
   };
-  
+
   return (
     <React.Fragment>
-      {!isValidSearch  && selectedOption==="name" ? (
-          <Stack sx={{ width: "100%" }} spacing={2}>
-            <Alert severity="error">
-              Please search for a name
-            </Alert>
-          </Stack>
-        ) : <></>}
-      {!isValidSearch  && selectedOption==="phoneNumber" ? (
-          <Stack sx={{ width: "100%" }} spacing={2}>
-            <Alert severity="error">
-              Please search for a phone number (123) 4567890
-            </Alert>
-          </Stack>
-        ) : <></>}
-      {!isValidSearch  && selectedOption==="invoiceId" ? (
-          <Stack sx={{ width: "100%" }} spacing={2}>
-            <Alert severity="error">
-              Please search for an invoice ID
-            </Alert>
-          </Stack>
-        ) : <></>}
+      {!isValidSearch && selectedOption === "name" ? (
+        <Stack sx={{ width: "100%" }} spacing={2}>
+          <Alert severity="error">Please search for a name</Alert>
+        </Stack>
+      ) : (
+        <></>
+      )}
+      {!isValidSearch && selectedOption === "phoneNumber" ? (
+        <Stack sx={{ width: "100%" }} spacing={2}>
+          <Alert severity="error">
+            Please search for a phone number (123) 4567890
+          </Alert>
+        </Stack>
+      ) : (
+        <></>
+      )}
+      {!isValidSearch && selectedOption === "invoiceId" ? (
+        <Stack sx={{ width: "100%" }} spacing={2}>
+          <Alert severity="error">Please search for an invoice ID</Alert>
+        </Stack>
+      ) : (
+        <></>
+      )}
       <div className={styles.center}>
         <CardWithRadius className={`${classes.blueCard} ${styles.inputBoxes}`}>
-          <input
-            type="radio"
-            value="invoiceId"
-            id="invoiceId"
-            name="selectedOption"
-            checked={selectedOption === "invoiceId"}
-            onChange={handleOptionChange}
-          />
-          <label>Invoice ID</label>
-
-          <br />
-
-          <input
-            type="radio"
-            value="phoneNumber"
-            name="selectedOption"
-            id="phoneNumber"
-            checked={selectedOption === "phoneNumber"}
-            onChange={handleOptionChange}
-          />
-          <label>Client Phone Number</label>
-          <br />
-
-          <input
-            type="radio"
-            value="name"
-            name="selectedOption"
-            id="name"
-            checked={selectedOption === "name"}
-            onChange={handleOptionChange}
-          />
-          <label>Client Name</label>
+          <FormControl>
+            <label>Search With</label>
+            <RadioGroup
+              row
+              aria-labelledby="demo-row-radio-buttons-group-label"
+              name="row-radio-buttons-group"
+            >
+              <FormControlLabel
+                value="name"
+                control={
+                  <Radio
+                    sx={{
+                      color: "white",
+                      "&.Mui-checked": {
+                        color: "#fabd44",
+                      },
+                    }}
+                  />
+                }
+                onChange={handleOptionChange}
+                label="Client Name"
+              />
+              <FormControlLabel
+                value="invoiceId"
+                control={
+                  <Radio
+                    sx={{
+                      color: "white",
+                      "&.Mui-checked": {
+                        color: "#fabd44",
+                      },
+                    }}
+                  />
+                }
+                onChange={handleOptionChange}
+                label="Invoice ID"
+              />
+              <FormControlLabel
+                value="phoneNumber"
+                control={
+                  <Radio
+                    sx={{
+                      color: "white",
+                      "&.Mui-checked": {
+                        color: "#fabd44",
+                      },
+                    }}
+                  />
+                }
+                onChange={handleOptionChange}
+                label="Phone Number"
+              />
+            </RadioGroup>
+          </FormControl>
         </CardWithRadius>
         <CardWithRadius className={`${classes.blueCard} ${styles.inputBoxes}`}>
-          <label>Search:</label>
-          <input
-            type="text"
-            className="SearchType"
-            required
-            value={searchInput}
-            onChange={handleSearchInput}
-          />
+          <Grid container spacing={0}>
+            <Grid item xs={11}>
+              <label>Search:</label>
+              <input
+                type="text"
+                className="SearchType"
+                required
+                value={searchInput}
+                onChange={handleSearchInput}
+              />
+            </Grid>
+            <Grid item xs={1}>
+              <Button className={buttonClass.searchIcon} onClick={findHandler}>
+                <SearchIcon></SearchIcon>
+              </Button>
+            </Grid>
+          </Grid>
         </CardWithRadius>
       </div>
-
-      <Button className={classes.yellowCard} onClick={findHandler}>
-        Find
-      </Button>
-
-      {results.map((results) => (
-        <SearchResult key={results.clientId} clientName={results.clientName} clientId={results.clientId}/>
-        
-      ))}
-
+      <Card className={cardClass.resultBox}>
+        {noResult ? "No result" : <></>}
+        {results.map((results) => (
+          <Stack direction="row">
+            <SearchResult
+              key={results.clientId}
+              clientName={results.clientName}
+              clientId={results.clientId}
+              personalEmail={results.clientContact.personalEmail}
+              phoneNumber={results.clientContact.personalContactNumber}
+              businessEmail={results.clientContact.businessEmail}
+            />
+          </Stack>
+        ))}
+      </Card>
     </React.Fragment>
   );
 };
--- amt-invoice/src/components/Search/SearchResult.js
+++ amt-invoice/src/components/Search/SearchResult.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import CardWithRadius from "../UI/CardWithRadius";
 import ClientPage from "../ClientPage/ClientPage";
-import classes from "../UI/CardWithRadius.module.css";
+import classes from "../UI/Button.module.css";
 import styles from "./SearchInputs.module.css";
 import Button from "../UI/Button";
+import Grid from "@mui/material/Grid";
 
 import { GetClientById } from "../../services/ClientServices";
 
 const SearchResult = (props) => {
-
   const nav = useNavigate();
 
   const fetchData = async () => {
@@ -18,24 +18,43 @@ const SearchResult = (props) => {
     localStorage.setItem("clientInfo", JSON.stringify(data));
   };
 
-  const clientSelectedHandler =async () => {
+  const clientSelectedHandler = async () => {
     await fetchData();
-    console.log(localStorage.getItem('clientInfo'));
-    nav("/clientPage")
+    console.log(localStorage.getItem("clientInfo"));
+    nav("/clientPage");
   };
-
-
-
 
   return (
     <React.Fragment>
       <div>
-          <Button
-            className={classes.blueCard}
-            onClick={clientSelectedHandler}
+        <Button
+          className={` ${classes.searchResults}`}
+          onClick={clientSelectedHandler}
+        >
+          <Grid
+            sx={{ textAlign: "left", "padding-left": "10px" }}
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
           >
-            {props.clientName}
-          </Button>
+            <Grid item xs={6}>
+              <b>Name</b>
+              {props.clientName}
+            </Grid>
+            <Grid item xs={6}>
+              <b>Business Email: </b>
+              {props.businessEmail}
+            </Grid>
+            <Grid item xs={6}>
+              <b>Personal Email</b>
+              {props.personalEmail}
+            </Grid>
+            <Grid item xs={6}>
+              <b>Phone Number</b>
+              {props.phoneNumber}
+            </Grid>
+          </Grid>
+        </Button>
       </div>
     </React.Fragment>
   );
