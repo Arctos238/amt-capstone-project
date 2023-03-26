5f50156540a408be3486c0f9d210be8936cdf01c
EvenSten <42704997+EvenSten@users.noreply.github.com>
2023-03-25 18:42:45 -0600

    Forgot to push this code so here it is

--- amt-invoice/src/components/Invoices/InvoiceProjectDocumentUpload.js
+++ amt-invoice/src/components/Invoices/InvoiceProjectDocumentUpload.js
@@ -7,6 +7,7 @@ import classes from "../UI/CardWithRadius.module.css";
 import CardWithRadius from "../UI/CardWithRadius";
 import Typography from '@material-ui/core/Typography';
 import { CreateNewImage } from '../../services/ImageServices';
+import { saveAs } from 'file-saver';
 
 const DocumentUpload = (props) => {
   const { projectId } = props;
@@ -19,7 +20,7 @@ const DocumentUpload = (props) => {
   const fileSelectedHandler = useCallback(event => {
     setSelectedFile(event.target.files[0]);
     console.log(event.target.files[0]);
-    console.log(event.target.files[0].name);
+    console.log(event.target.files[0].url);
     const filename = event.target.files[0].name;
     console.log(filename);
     setFileArray(prevFiles => [...prevFiles, filename]); 
@@ -35,18 +36,40 @@ const DocumentUpload = (props) => {
     console.log(data);
   };
 
+  const donwloadImg =() => {
+    saveAs()
+  }
+
   const fileComponents = fileArray.map((file, index) => (
-    <CardWithRadius key={index} className={classes.blueCard}>
+    <CardWithRadius key={index} className={classes.blueCard} onClick={() => downloadFile(file)}>
       <Typography style={{ textAlign: 'center', fontWeight: 'bold' }}>{file}</Typography>
     </CardWithRadius>
   ));
 
+  const downloadFile = async (filename) => {
+    try {
+      const response = await axios.get(`/files/${filename}`, {
+        responseType: 'blob'
+      });
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', filename);
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div>
-      <Button variant="contained" component="label">
+      <Button variant="contained" component="label" style={{ backgroundColor: '#05516a' }}>
         Add Image
         <input hidden type="file" onChange={fileSelectedHandler} />
+        <img></img>
         <PhotoCamera />
       </Button>
       <br />
