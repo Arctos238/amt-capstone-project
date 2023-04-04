d8a24d7d6cb9eb33dd7051420431ce17958d18e6
EvenSten <42704997+EvenSten@users.noreply.github.com>
2023-04-03 20:41:12 -0600

    Not yet finished, I am very close just need to finsish a couple of details before its all done

--- amt-invoice/src/components/Invoices/WorkOrderForm.js
+++ amt-invoice/src/components/Invoices/WorkOrderForm.js
@@ -2,86 +2,142 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import BackButton from "../BackButton/BackButton";
 import { useState, useEffect, useRef } from "react";
-import { TableBody, TableCell, TableContainer, TableRow, Paper } from '@mui/material';
+import { TableBody, TableCell, TableContainer, TableRow, Paper, TableHead, Table } from '@mui/material';
 import ReactToPrint from "react-to-print";
-import { Button } from "react-bootstrap";
+import { Button, Icon } from '@mui/material';
+import PrintIcon from '@mui/icons-material/Print';
 
-export default function WorkOrderForm (props) {
-    
-    let componentRef = useRef();
-    const [items, setItems] = useState({});
-        const currentDate = new Date().toLocaleDateString();
-        let clientInfo = localStorage.getItem("clientInfo");
-        let toArray = JSON.parse(clientInfo);
-        console.log(toArray[0]);
+export default function WorkOrderForm(props) {
+  const groceryItems = ["apples", "bananas", "milk", "bread"];
+  let componentRef = useRef();
+  const [items, setItems] = useState({});
+  const currentDate = new Date().toLocaleDateString();
+  let clientInfo = localStorage.getItem("clientInfo");
+  let toArray = JSON.parse(clientInfo);
+  console.log(toArray[0]);
 
-        useEffect(() => {
-            const items = JSON.parse(localStorage.getItem('invoice'));
-            if (items) {
-             setItems(items);
-            }
-          }, []);
-    
-         console.log(items.invoices);
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("invoice"));
+    if (items) {
+      setItems(items);
+    }
+  }, []);
 
-    return(
-        <div>
-            <BackButton/>
+  if (items && items.invoices && items.invoices.length > 0) {
+    console.log(items.invoices[0].invoiceItems);
+  } else {
+    console.log('No invoices found.');
+  }
 
-            <div style={{ maxWidth: "800px", margin: "0 auto" }}>
-            <TableContainer component={Paper}>
+  
+  
+
+  return (
+    <div>
+    <BackButton />
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div style={{ maxWidth: "800px" }}>
+        
+
+        <div style={{ margin: "0 auto" }}>
+          <TableContainer component={Paper}>
             <TableBody>
-            <TableRow>
-            <TableCell style={{ fontWeight: "bold" }}>Contractor</TableCell>
+              <TableRow>
+                <TableCell style={{ fontWeight: "bold" }}>Contractor</TableCell>
                 <TableCell>{toArray[0].clientName}</TableCell>
                 <TableCell style={{ fontWeight: "bold" }}>Phone</TableCell>
                 <TableCell>{toArray[0].clientContact.personalContactNumber}</TableCell>
                 <TableCell style={{ fontWeight: "bold" }}>Date</TableCell>
                 <TableCell>{currentDate}</TableCell>
-            </TableRow> 
-            <TableRow>
+              </TableRow>
+              <TableRow>
                 <TableCell style={{ fontWeight: "bold" }}>Project</TableCell>
                 <TableCell>{items.projectName}</TableCell>
                 <TableCell style={{ fontWeight: "bold" }}>Order NO</TableCell>
                 <TableCell></TableCell>
                 <TableCell style={{ fontWeight: "bold" }}>Page NO</TableCell>
-                
-            </TableRow> 
-            <TableRow>
+                <TableCell/>
+              </TableRow>
+              <TableRow>
                 <TableCell style={{ fontWeight: "bold" }}>Address</TableCell>
                 <TableCell>{items.projectAddress?.firstLineAddress}</TableCell>
                 <TableCell style={{ fontWeight: "bold" }}>Postal Code</TableCell>
                 <TableCell>{items.projectAddress?.postalCode}</TableCell>
                 <TableCell style={{ fontWeight: "bold" }}>Required</TableCell>
                 <TableCell>{currentDate}</TableCell>
-            </TableRow> 
-            <TableRow>
+              </TableRow>
+              <TableRow>
                 <TableCell style={{ fontWeight: "bold" }}>Material</TableCell>
                 <TableCell>See List</TableCell>
-                <TableCell style={{ fontWeight: "bold" }}></TableCell>
-                <TableCell></TableCell>
                 <TableCell style={{ fontWeight: "bold" }}>Invoice #</TableCell>
                 <TableCell>{items.invoices?.[0]?.invoiceId}</TableCell>
-            </TableRow> 
+                <TableCell style={{ fontWeight: "bold" }}>Invoiced</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
             </TableBody>
-        </TableContainer>
-        
-        <TableContainer>
+          </TableContainer>
 
+          <br></br>
+
+          <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell style={{ borderRight: "1px solid #ddd", fontWeight: "bold" }}>PC. NO.</TableCell>
+                <TableCell style={{ borderRight: "1px solid #ddd", fontWeight: "bold" }}>NO. OF PCS</TableCell>
+                <TableCell style={{ borderRight: "1px solid #ddd", fontWeight: "bold" }}>SIZE</TableCell>
+                <TableCell style={{ borderRight: "1px solid #ddd", fontWeight: "bold" }}>UNIT</TableCell>
+                <TableCell style={{ borderRight: "1px solid #ddd", fontWeight: "bold" }}>EDGE NO.</TableCell>
+                <TableCell style={{ fontWeight: "bold" }}>M2/SQ. FT.</TableCell>
+              </TableRow>
+            </TableHead>
+            
+            <TableBody>
+            {items && items.invoices && items.invoices.length > 0 && items.invoices[0].invoiceItems.length > 0 ? (
+              items.invoices[0].invoiceItems.map((item) => (
+                <TableRow key={item.id}>
+                <TableCell style={{ borderRight: "1px solid #ddd"}}></TableCell>
+                <TableCell style={{ borderRight: "1px solid #ddd"}}></TableCell>
+                <TableCell style={{ borderRight: "1px solid #ddd"}}></TableCell>
+                <TableCell style={{ borderRight: "1px solid #ddd", fontWeight: "bold" }}>UNIT</TableCell>
+                <TableCell style={{ borderRight: "1px solid #ddd", fontWeight: "bold" }}>EDGE NO.</TableCell>
+                <TableCell style={{ fontWeight: "bold" }}>M2/SQ. FT.</TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={5}>No invoices found.</TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+
+
+
+
+
+          </Table>
         </TableContainer>
-        </div>  
-    
+        </div>
+
+        <br>
+        </br>
+
         <ReactToPrint
-          trigger={() => <Button>Print this out!</Button>}
+          trigger={() => (
+            <Button variant="contained" startIcon={<PrintIcon />}>
+              Print
+            </Button>
+          )}
           content={() => componentRef}
         />
 
         <div style={{ display: "none" }}>
           <ComponentToPrint ref={(el) => (componentRef = el)} />
         </div>
-
+      </div>
     </div>
-    );
+    </div>
+  );
 }
 
 class ComponentToPrint extends React.Component {
@@ -140,12 +196,12 @@ class ComponentToPrint extends React.Component {
                 <TableCell>{currentDate}</TableCell>
             </TableRow> 
             <TableRow>
-                <TableCell style={{ fontWeight: "bold" }}>Material</TableCell>
+            <TableCell style={{ fontWeight: "bold" }}>Material</TableCell>
                 <TableCell>See List</TableCell>
-                <TableCell style={{ fontWeight: "bold" }}></TableCell>
-                <TableCell></TableCell>
                 <TableCell style={{ fontWeight: "bold" }}>Invoice #</TableCell>
                 <TableCell>{items.invoices?.[0]?.invoiceId}</TableCell>
+                <TableCell style={{ fontWeight: "bold" }}>Invoiced</TableCell>
+                <TableCell></TableCell>
             </TableRow> 
             </TableBody>
           </TableContainer>
@@ -155,47 +211,3 @@ class ComponentToPrint extends React.Component {
   }
   
   
-
-
-//   <div style={{ maxWidth: "800px", margin: "0 auto" }}>
-//         <TableContainer component={Paper}>
-//             <TableBody>
-//             <TableRow>
-//             <TableCell style={{ fontWeight: "bold" }}>Contractor</TableCell>
-//                 <TableCell>{toArray[0].clientName}</TableCell>
-//                 <TableCell style={{ fontWeight: "bold" }}>Phone</TableCell>
-//                 <TableCell>{toArray[0].clientContact.personalContactNumber}</TableCell>
-//                 <TableCell style={{ fontWeight: "bold" }}>Date</TableCell>
-//                 <TableCell>{currentDate}</TableCell>
-//             </TableRow> 
-            // <TableRow>
-            //     <TableCell style={{ fontWeight: "bold" }}>Project</TableCell>
-            //     <TableCell>{items.projectName}</TableCell>
-            //     <TableCell style={{ fontWeight: "bold" }}>Order NO</TableCell>
-            //     <TableCell></TableCell>
-            //     <TableCell style={{ fontWeight: "bold" }}>Page NO</TableCell>
-                
-            // </TableRow> 
-            // <TableRow>
-            //     <TableCell style={{ fontWeight: "bold" }}>Address</TableCell>
-            //     <TableCell>{items.projectAddress?.firstLineAddress}</TableCell>
-            //     <TableCell style={{ fontWeight: "bold" }}>Postal Code</TableCell>
-            //     <TableCell>{items.projectAddress?.postalCode}</TableCell>
-            //     <TableCell style={{ fontWeight: "bold" }}>Required</TableCell>
-            //     <TableCell>{currentDate}</TableCell>
-            // </TableRow> 
-            // <TableRow>
-            //     <TableCell style={{ fontWeight: "bold" }}>Material</TableCell>
-            //     <TableCell>See List</TableCell>
-            //     <TableCell style={{ fontWeight: "bold" }}></TableCell>
-            //     <TableCell></TableCell>
-            //     <TableCell style={{ fontWeight: "bold" }}>Invoice #</TableCell>
-            //     <TableCell>{items.invoices?.[0]?.invoiceId}</TableCell>
-            // </TableRow> 
-//             </TableBody>
-//         </TableContainer>
-        
-//         <TableContainer>
-
-//         </TableContainer>
-//         </div>  
