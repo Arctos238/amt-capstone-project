5f0e5c12c2eb99a63547c02a98bcff3537f1d365
EvenSten <42704997+EvenSten@users.noreply.github.com>
2023-04-13 12:39:55 -0600

    updating color

--- amt-invoice/src/components/Create_client/CreateClientDetails.js
+++ amt-invoice/src/components/Create_client/CreateClientDetails.js
@@ -2,111 +2,124 @@ import React from "react";
 import { useState, useEffect } from "react";
 import CardWithRadius from "../UI/CardWithRadius";
 import classes from "../UI/CardWithRadius.module.css";
-import styles from"./CreateClientDetails.module.css"
+import styles from "./CreateClientDetails.module.css"
 import TextField from '@mui/material/TextField';
 import { MuiTelInput } from 'mui-tel-input'
 
 
-const CreateClientDetails = (props) =>{
-    const [value, setValue] = React.useState("+1")
+
+
+const CreateClientDetails = (props) => {
+
+  const [value, setValue] = React.useState("+1")
 
   const handleChange = (newValue) => {
 
     setValue(newValue)
   }
-    
 
-const changeToHome = () => {
-    
-}
 
-return (
-    
+  const changeToHome = () => {
+
+  }
+
+  return (
+
 
 
     <React.Fragment>
-        <div className = {styles.center} >
-            <CardWithRadius className={`${classes.blueCard} ${styles.inputBoxes}`}>
-            <TextField
-            sx={{ width: "100%", color: "white", "& .MuiInputBase-input": {
+      <div className={styles.center} >
+        <CardWithRadius className={`${classes.blueCard} ${styles.inputBoxes}`}>
+          <TextField
+            sx={{
+              width: "100%", color: "white", "& .MuiInputBase-input": {
                 width: "calc(100% - 2px)", // 2px is the width of the underline
-              }, }}
+              },
+            }}
             id="standard-basic"
             label="Client Name"
             placeholder="Candler Bing"
             variant="standard"
-            type = "text" 
-            inputRef= {props.clientNameRef}
+            type="text"
+            inputRef={props.clientNameRef}
             InputLabelProps={{ className: styles.textFieldLabel }}
             InputProps={{
               classes: {
-                underline: styles.underline, 
+                underline: styles.underline,
               },
             }}
           />
-            {/* Keeping this just in case what is above doesn't work  */}
-            {/* <label>Client Name:</label>
+          {/* Keeping this just in case what is above doesn't work  */}
+          {/* <label>Client Name:</label>
             <input type = "text" className={styles.inputBox}
             ref= {props.clientNameRef}
             /> */}
-            </CardWithRadius>
-            
-            <CardWithRadius className={`${classes.blueCard} ${styles.inputBoxes}`}>
-            <TextField
-            sx={{ width: "100%", color: "white", "& .MuiInputBase-input": {
+        </CardWithRadius>
+
+        <CardWithRadius className={`${classes.blueCard} ${styles.inputBoxes}`}>
+          <TextField
+            sx={{
+              width: "100%", color: "white", "& .MuiInputBase-input": {
                 width: "calc(100% - 2px)", // 2px is the width of the underline
-              }, }}
+              },
+            }}
             id="standard-basic"
             label="Email Personal"
             placeholder="RonSwanson@no.com"
             variant="standard"
-            type = "text" 
-            inputRef= {props.emailPersonalRef}
+            type="text"
+            inputRef={props.emailPersonalRef}
             InputLabelProps={{ className: styles.textFieldLabel }}
             InputProps={{
               classes: {
-                underline: styles.underline, 
+                underline: styles.underline,
               },
             }}
-          /> 
-            {/* Keeping this just in case what is above doesn't work 
+          />
+          {/* Keeping this just in case what is above doesn't work 
             <label>Email Personal:</label>
             <input type = "text" className={styles.inputBox}
             ref= {props.emailPersonalRef}
             /> */}
-            </CardWithRadius>
-            <CardWithRadius className={`${classes.blueCard} ${styles.inputBoxes}`}>
-            <TextField
-            sx={{ width: "100%", color: "white", "& .MuiInputBase-input": {
+        </CardWithRadius>
+        <CardWithRadius className={`${classes.blueCard} ${styles.inputBoxes}`}>
+          <TextField
+            sx={{
+              width: "100%", color: "white", "& .MuiInputBase-input": {
                 width: "calc(100% - 2px)", // 2px is the width of the underline
-              }, }}
+              },
+            }}
             id="standard-basic"
             label="Email Business"
             placeholder="Business@work.ca"
             variant="standard"
-            type = "text" 
-            inputRef= {props.emailBusinessRef}
+            type="text"
+            inputRef={props.emailBusinessRef}
             InputLabelProps={{ className: styles.textFieldLabel }}
             InputProps={{
               classes: {
-                underline: styles.underline, 
+                underline: styles.underline,
               },
             }}
-          />    
-            {/* Keeping this just in case what is above doesn't work
+          />
+          {/* Keeping this just in case what is above doesn't work
             <label>Email Business:</label>
             <input type = "text" className={styles.inputBox}
             ref= {props.emailBusinessRef}
             /> */}
-            </CardWithRadius>
-            <CardWithRadius className={`${classes.blueCard} ${styles.inputBoxes}`}>
-            <MuiTelInput 
-             pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-             inputRef= {props.phoneNumberRef}
-             type="tel" 
-             value={value} onChange={handleChange} />
-            
-            {/* Keeping just in case
+        </CardWithRadius>
+        <CardWithRadius className={`${classes.blueCard} ${styles.inputBoxes}`}>
+          <MuiTelInput
+            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+            inputRef={props.phoneNumberRef}
+            type="tel"
+            value={value} onChange={handleChange}
+            inputProps={{
+              className: styles.inputPhone,
+            }} />
+
+
+          {/* Keeping just in case
             <label>Phone Number:</label>
             <input  type="tel" 
               className={styles.inputBox}
@@ -114,10 +127,10 @@ return (
               pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
               ref= {props.phoneNumberRef}
             /> */}
-            </CardWithRadius>
-        </div>
+        </CardWithRadius>
+      </div>
     </React.Fragment>
-);
+  );
 };
 
 export default CreateClientDetails;
