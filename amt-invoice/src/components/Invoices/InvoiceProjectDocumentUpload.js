48959e1728ffb4a6050fd7c3bc42af7b530f8826
Jacque Pointer <93753375+Arctos238@users.noreply.github.com>
2023-04-12 21:30:31 -0600

    Fixed upload Images

--- amt-invoice/src/components/Invoices/InvoiceProjectDocumentUpload.js
+++ amt-invoice/src/components/Invoices/InvoiceProjectDocumentUpload.js
@@ -1,13 +1,13 @@
-import React, { useState, useCallback } from 'react';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import PhotoCamera from '@mui/icons-material/PhotoCamera';
-import axios from 'axios';
+import React, { useState, useCallback } from "react";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import axios from "axios";
 import classes from "../UI/CardWithRadius.module.css";
 import CardWithRadius from "../UI/CardWithRadius";
-import Typography from '@material-ui/core/Typography';
-import { CreateNewImage } from '../../services/ImageServices';
-import { saveAs } from 'file-saver';
+import Typography from "@material-ui/core/Typography";
+import { CreateNewImage } from "../../services/ImageServices";
+import { saveAs } from "file-saver";
 
 const DocumentUpload = (props) => {
   const { projectId } = props;
@@ -15,23 +15,23 @@ const DocumentUpload = (props) => {
   const [file, setFile] = useState();
   const [fileArray, setFileArray] = useState([]);
 
-
   console.log(file);
   console.log(projectId);
 
-  const fileSelectedHandler = useCallback(event => {
+  const fileSelectedHandler = useCallback((event) => {
     setSelectedFile(event.target.files[0]);
     console.log(event.target.files[0]);
     console.log(event.target.files[0]);
     const filename = event.target.files[0].name;
     console.log(filename);
-    setFileArray(prevFiles => [...prevFiles, filename]);
+    setFileArray((prevFiles) => [...prevFiles, filename]);
     setFile(URL.createObjectURL(event.target.files[0]));
   }, []);
 
   const fileUploadHandler = async () => {
     const fd = new FormData();
-    fd.append('file', selectedFile);
+    fd.append("file", selectedFile);
+    fd.append("projectId", projectId);
 
     const data = await CreateNewImage(fd);
     console.log(projectId);
@@ -39,12 +39,14 @@ const DocumentUpload = (props) => {
   };
 
   const donwloadImg = () => {
-    saveAs()
-  }
+    saveAs();
+  };
 
   const fileComponents = fileArray.map((file, index) => (
     <CardWithRadius key={index} className={classes.blueCard}>
-      <Typography style={{ textAlign: 'center', fontWeight: 'bold' }}>{file}</Typography>
+      <Typography style={{ textAlign: "center", fontWeight: "bold" }}>
+        {file}
+      </Typography>
       <Button onClick={() => downloadFile(file)}>Download</Button>
     </CardWithRadius>
   ));
@@ -55,16 +57,18 @@ const DocumentUpload = (props) => {
 
   return (
     <div>
-      <Button variant="contained" component="label" style={{ backgroundColor: '#05516a' }}>
+      <Button
+        variant="contained"
+        component="label"
+        style={{ backgroundColor: "#05516a" }}
+      >
         Add Image
         <input hidden type="file" onChange={fileSelectedHandler} />
-
         <PhotoCamera />
       </Button>
       <br />
 
       {fileComponents}
-
 
       <br />
       <button onClick={fileUploadHandler}>Upload Image</button>
